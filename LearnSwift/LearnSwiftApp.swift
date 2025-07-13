1d04252443bcb6cef6a0d54f2ac2dd1e08828fed
时雨 <270598250@qq.com>
2025-07-13 22:18:34 +0800

    添加当前项目进度

---
+++ LearnSwift/LearnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftApp.swift
+//  LearnSwift
+//
+//  Created by 时雨 on 2025/7/13.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
